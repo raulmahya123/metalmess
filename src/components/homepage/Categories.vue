e09ff5c0277b81d2f3a123de9a6a27638df18493
raulmahya <raulmahya11@gmail.com>
2023-04-30 13:55:49 +0700

    push naikin revisi agar codingan lebih dinamis dan tdk statis

---
+++ src/components/homepage/Categories.vue
@@ -0,0 +1,23 @@
+<script setup>
+import categoriesCard from '../categoriesCard.vue';
+import {ref} from 'vue';
+const categories = ref([
+    {id : 1,title: "Mobile UI", count:"731", Image:"categories-1.jpg"},
+    {id : 2,title: "Fonts", count:"657", Image:"categories-2.jpg"},
+    {id : 3,title: "Icon Set" ,count:"83559", Image:"categories-3.jpg"},
+    {id : 4,title: "Website UI Kit", count:"4500", Image:"categories-4.jpg"} 
+])
+</script>
+
+<template>
+      <div class="container px-4 mx-auto my-16 md:px-12">
+    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+        <categoriesCard v-for="category in categories" 
+        :key="category.id" 
+        :title="category.title" 
+        :count="category.count"
+         :Image="category.Image" />
+    </div>
+</div>
+</template>
---
+++ src/components/layouts/Logo.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+
+                <RouterLink to="index.html" class="flex items-center">
+                <img src="@/assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-8" alt="Flowbite Logo" />
+            </RouterLink>
+
+</template>
