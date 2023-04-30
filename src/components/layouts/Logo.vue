12e5b266760c413ab3d7cd406c9c67fa53d982ae
raulmahya <raulmahya11@gmail.com>
2023-04-30 14:18:40 +0700

    revisi bagian logo

--- src/components/layouts/Logo.vue
+++ src/components/layouts/Logo.vue
@@ -6,7 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
 
                 <RouterLink to="index.html" class="flex items-center">
-                <img src="@/assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-8" alt="Flowbite Logo" />
+                <img src="@/assets/img/metalmes.png" class="h-8 ml-3 sm:ml-0 sm:h-8" alt="Flowbite Logo" />
             </RouterLink>
 
 </template>
