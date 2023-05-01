2c204dbaa6748e2e26b10e0f84d8e46e47558e3c
raulmahya <raulmahya11@gmail.com>
2023-05-01 11:50:56 +0700

    asdas

--- src/components/homepage/Categories.vue
+++ src/components/homepage/Categories.vue
@@ -13,11 +13,11 @@ const categories = ref([
       <div class="container px-4 mx-auto my-16 md:px-12">
     <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
-        <categoriesCard v-for="category in categories" 
-        :key="category.id" 
-        :title="category.title" 
-        :count="category.count"
-         :Image="category.Image" />
+        <categoriesCard v-for="categories in categories" 
+        :key="categories.id" 
+        :title="categories.title" 
+        :count="categories.count"
+         :Image="categories.Image" />
     </div>
 </div>
 </template>
