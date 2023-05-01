72632aeba11dbb19d147abda24ce67a2991d0aa2
raulmahya <raulmahya11@gmail.com>
2023-05-01 11:46:54 +0700

    revisi gambar

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -13,7 +13,7 @@ import Login from '../components/autentication/Login.vue';
             <Login/>
           </div>
           <div class="w-full mx-auto mt-20 sm:max-w-2xl">
-            <img src="img/brand-logo.svg" alt="" class="w-full px-10 md:px-0" />
+            <img src="@/assets/img/brand-logo.svg" alt="" class="w-full px-10 md:px-0" />
           </div>
         </div>
       </div>
